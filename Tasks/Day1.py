6582d5ea01d28e7015c301e67de398ac426b4786
Khadeejathul Humaira .M.S <55457122+KhadeejathulHumaira@users.noreply.github.com>
2020-11-23 13:37:10 +0530

    Add files via upload

---
+++ Tasks/Day1.py
@@ -0,0 +1,14 @@
+print("30 days 30 hours challenge")
+Hours = "Thirty"
+print(Hours)
+a = "First"
+b = "Day"
+c = a + b
+print(c)
+First = "This is FIRST the first task"
+case = First.casefold() #casefold() will change every capital letter to small letter
+print(case)
+## Todays task
+## slicing the string
+Task="This is the first challenge"
+print(Task[1:])
