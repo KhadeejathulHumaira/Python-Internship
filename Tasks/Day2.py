ffe643668fc37faa1eff4dd7b3f55a693afc34e5
Khadeejathul Humaira .M.S <55457122+KhadeejathulHumaira@users.noreply.github.com>
2020-11-23 22:26:29 -0800

    Update Day2.py

--- Tasks/Day2.py
+++ Tasks/Day2.py
@@ -16,7 +16,7 @@ print(f"{x},{y},{z}")
 a=b=c=40
 print(f"Divide a by 10:{a/10}\nDivide a by 10:{a//10}\nMultiply b by 50:{b*50}\nAdd c value by 60:{c+60}")
 string="Apple"
-print(string[:2]+"a"+string[3:])
+print(string[:2]+"G"+string[3:])
 m=5
 n=6.00
 print(f"{int(n)}")
