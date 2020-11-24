90b887ab3849b88f88a92e506f486b5bf355b363
Khadeejathul Humaira .M.S <55457122+KhadeejathulHumaira@users.noreply.github.com>
2020-11-23 22:25:20 -0800

    Add files via upload

---
+++ Tasks/Day2.py
@@ -0,0 +1,25 @@
+#Day 2 -Variables,
+a1=5  #"=" Will assign the value to the variable
+b1=1.25
+c1="BestEnlist"
+d1=5+8j
+print(a1, type(a1))
+print(b1, type(b1))
+print(c1, type(c1))
+print(d1, type(d1))
+
+# Multi variable
+x,y,z=1,2.5,"Best"
+print(f"{x},{y},{z}")
+
+#Day 2 -Task
+a=b=c=40
+print(f"Divide a by 10:{a/10}\nDivide a by 10:{a//10}\nMultiply b by 50:{b*50}\nAdd c value by 60:{c+60}")
+string="Apple"
+print(string[:2]+"a"+string[3:])
+m=5
+n=6.00
+print(f"{int(n)}")
+print(f"{float(m)}")
+
+
