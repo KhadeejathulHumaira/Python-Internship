f28d28fc78e37fe5e003ca15fcad228fc08c5aa7
Khadeejathul Humaira .M.S <55457122+KhadeejathulHumaira@users.noreply.github.com>
2020-11-26 23:05:37 -0800

    Update Day5.py

--- Tasks/Day5.py
+++ Tasks/Day5.py
@@ -2,11 +2,11 @@
 #Day5
 #1)
 def calculator(a,b):
-    print(f"Addition of two number is:{a+b}\nSubraction of two number is:{a-b}\nDivision of two number is:{a//b}\nMultiplication of two number is:{a*b}")
+    print(f"Addition of two numbers is:{a+b}\nSubraction of two numbers is:{a-b}\nDivision of two numbers is:{a//b}\nMultiplication of two numbers is:{a*b}")
 a=int(input("Enter the first value:"))
 b=int(input("Enter the second value:"))
 calculator(a,b)
-
+#2)
 def covid(name,body_temperature=98):
     print(f"Name:"+name,"\nBody Temperature:"+str(body_temperature))
 covid("Harry")
